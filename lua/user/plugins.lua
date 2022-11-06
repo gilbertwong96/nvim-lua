cb425d2c4baa4770bd8870afc14b9a0117d06408
Gilbert <gilbertwong96@icloud.com>
2022-11-06 15:28:22 +0800

    Introduce faster.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,6 +60,21 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
   use "sheerun/vim-polyglot"
   use "gpanders/editorconfig.nvim"
+  -- faster nvim
+  use {
+    "PHSix/faster.nvim",
+    event = { "VimEnter *" },
+    config = function()
+      vim.api.nvim_set_keymap('n', 'j', '<Plug>(faster_move_j)', { noremap = false, silent = true })
+      vim.api.nvim_set_keymap('n', 'k', '<Plug>(faster_move_k)', { noremap = false, silent = true })
+      -- or
+      -- vim.api.nvim_set_keymap('n', 'j', '<Plug>(faster_move_gj)', {noremap=false, silent=true})
+      -- vim.api.nvim_set_keymap('n', 'k', '<Plug>(faster_move_gk)', {noremap=false, silent=true})
+      -- if you need map in visual mode
+      -- vim.api.nvim_set_keymap('v', 'j', '<Plug>(faster_vmove_j)', {noremap=false, silent=true})
+      -- vim.api.nvim_set_keymap('v', 'k', '<Plug>(faster_vmove_k)', {noremap=false, silent=true})
+    end
+  }
 
   -- Colorschemes
   use "tanvirtin/monokai.nvim"
