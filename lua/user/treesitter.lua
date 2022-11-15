e68066c42645d95ac650daf04763333ef5ccb985
Gilbert <gilbertwong96@icloud.com>
2022-11-15 11:58:51 +0800

    Disable erlang treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -8,7 +8,7 @@ configs.setup {
   },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
+    disable = { "erlang" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
 
   },
