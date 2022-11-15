f3fd032e11b07165ff28517d3de490430a5b7da6
Gilbert <gilbertwong96@icloud.com>
2022-11-15 11:58:09 +0800

    Replace faster.nvim with accelerate-jk.nvim

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -65,3 +65,8 @@ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+
+-- accelerated-nvim
+vim.api.nvim_set_keymap('n', 'j', '<Plug>(accelerated_jk_gj)', {})
+vim.api.nvim_set_keymap('n', 'k', '<Plug>(accelerated_jk_gk)', {})
+
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,21 +72,8 @@ return packer.startup(function(use)
     opt = true
   }
 
-  -- faster nvim
-  use {
-    "PHSix/faster.nvim",
-    event = { "VimEnter *" },
-    config = function()
-      vim.api.nvim_set_keymap('n', 'j', '<Plug>(faster_move_j)', { noremap = false, silent = true })
-      vim.api.nvim_set_keymap('n', 'k', '<Plug>(faster_move_k)', { noremap = false, silent = true })
-      -- or
-      -- vim.api.nvim_set_keymap('n', 'j', '<Plug>(faster_move_gj)', {noremap=false, silent=true})
-      -- vim.api.nvim_set_keymap('n', 'k', '<Plug>(faster_move_gk)', {noremap=false, silent=true})
-      -- if you need map in visual mode
-      -- vim.api.nvim_set_keymap('v', 'j', '<Plug>(faster_vmove_j)', {noremap=false, silent=true})
-      -- vim.api.nvim_set_keymap('v', 'k', '<Plug>(faster_vmove_k)', {noremap=false, silent=true})
-    end
-  }
+  -- accelerated-jk nvim
+  use "rainbowhxch/accelerated-jk.nvim"
 
   -- Colorschemes
   use "tanvirtin/monokai.nvim"
